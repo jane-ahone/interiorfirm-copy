d585f6211d7906e974340f57d57bc93d5abd241a
Jane Ahone <jane.eloundou@gmail.com>
2025-07-16 03:03:46 +0100

    readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/layout/Footer/page";
 import DreamSpace from "@/components/organisms/DreamSpace/page";
-import ExperienceSection from "@/components/organisms/Experience/Experience";
 import Feedback from "@/components/organisms/Feedback/page";
 import Hero from "@/components/organisms/Hero";
 import InnovativeSection from "@/components/organisms/Innovative/page";
@@ -12,7 +11,7 @@ export default function Home() {
   return (
     <main className="container-grid">
       <Hero />
-      <ExperienceSection />
+      {/* <ExperienceSection /> */}
       <DreamSpace />
       <InnovativeSection />
       <Unique />
