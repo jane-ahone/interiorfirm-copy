8eaa9de60384933dfbed7140e79f665531a58a71
Jane Ahone <jane.eloundou@gmail.com>
2025-07-19 03:39:53 +0100

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
-import Footer from "@/components/layout/Footer/page";
-import DreamSpace from "@/components/organisms/DreamSpace/page";
-import ExperienceSection from "@/components/organisms/Experience/experience";
-import Feedback from "@/components/organisms/Feedback/page";
 import Hero from "@/components/organisms/Hero";
-import Interior from "@/components/organisms/Interior/page";
-import Unique from "@/components/organisms/Unique/page";
-import Working from "@/components/organisms/Working/page";
 
 export default function Home() {
   return (
