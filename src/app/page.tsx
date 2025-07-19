e166723bfd53ad0b3a466655afa5b69b3a7ed36f
Jane Ahone <jane.eloundou@gmail.com>
2025-07-19 03:04:17 +0100

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import DreamSpace from "@/components/organisms/DreamSpace/page";
 import ExperienceSection from "@/components/organisms/Experience/experience";
 import Feedback from "@/components/organisms/Feedback/page";
 import Hero from "@/components/organisms/Hero";
-import InnovativeSection from "@/components/organisms/Innovative/page";
 import Interior from "@/components/organisms/Interior/page";
 import Unique from "@/components/organisms/Unique/page";
 import Working from "@/components/organisms/Working/page";
