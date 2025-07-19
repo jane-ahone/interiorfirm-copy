115be955bfcff56333c3c72b00ad1285e3b19c62
Jane Ahone <jane.eloundou@gmail.com>
2025-07-19 03:42:56 +0100

    add exp section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ExperienceSection from "@/components/organisms/Experience/experience";
 import Hero from "@/components/organisms/Hero";
 
 export default function Home() {
   return (
     <main className="container-grid">
       <Hero />
+      <ExperienceSection/>
       {/* <ExperienceSection />
       <DreamSpace />
       <InnovativeSection />
